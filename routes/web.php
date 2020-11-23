382c3b8e45fa6b047b16c93462372111f356fb54
Jochen Sengier <jochen@celcius.be>
2020-11-23 08:52:54 +0100

    Forgot why I parsed the phpinfo() instead of just calling phpversion()

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,5 @@
 <?php
 
 Route::get('vursion', function () {
-	ob_start();
-	phpinfo(1);
-	$success = preg_match('/\bphp version ([A-Za-z0-9\.\-\+]*)\b/i', ob_get_contents(), $matches);
-	ob_end_clean();
-
-	if ($success) {
-		return response()->json($matches[1]);
-	}
+	return response()->json(phpversion());
 })->name('vursion')->middleware((version_compare(app()->version(), '5.6.12') >= 0) ? 'signed' : null);
