52d68a473fde3ef2d0eaf54a608cc6cf2218f0e5
Jochen Sengier <jochen@celcius.be>
2023-04-11 13:45:47 +0200

    Add useful info to the about command on Laravel installations >= 9.21

--- src/VursionServiceProvider.php
+++ src/VursionServiceProvider.php
@@ -26,9 +26,15 @@ class VursionServiceProvider extends ServiceProvider
 
         if ((version_compare(app()->version(), '9.21') >= 0) && class_exists(\Illuminate\Foundation\Console\AboutCommand::class)) {
             \Illuminate\Foundation\Console\AboutCommand::add('Vursion', [
-                'Enabled' => fn() => (config('vursion.enabled') ? '<fg=green>TRUE</>' : '<fg=yellow>FALSE</>'),
-                'Key'     => fn() => config('vursion.key'),
-                'Route'   => fn() => ((version_compare(app()->version(), '5.6.12') >= 0) ? \Illuminate\Support\Facades\URL::signedRoute('vursion') : route('vursion')),
+                'Enabled' => function () {
+                    return (config('vursion.enabled') ? '<fg=yellow>TRUE</>' : '<fg=yellow>FALSE</>');
+                },
+                'Key'     => function () {
+                    return config('vursion.key');
+                },
+                'Route'   => function () {
+                    return ((version_compare(app()->version(), '5.6.12') >= 0) ? \Illuminate\Support\Facades\URL::signedRoute('vursion') : route('vursion'));
+                },
             ]);
         }
     }
